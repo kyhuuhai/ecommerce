1d91e6ce7ade418ce4761f7b1e09035a531ae4ba
Do Vu Quan <dovuquan1995@gmail.com>
2017-06-20 11:37:53 +0700

    design

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20170620025108) do
 
   create_table "cart_details", force: :cascade do |t|
@@ -79,5 +80,6 @@ ActiveRecord::Schema.define(version: 20170620025108) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 
 end
