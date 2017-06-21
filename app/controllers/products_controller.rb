a851a9d0bdb665457934448a94839d49391823c7
krylot <kyhuuhai@gmai.com>
2017-06-21 12:03:43 +0700

    CRUD product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,57 @@
-class ProductsController < ActionController::Base
-	def index
- 	 	@products = Product.all
- 	 	@categories = Category.all
-  	end
+class ProductsController < ApplicationController
+  before_action :load_product, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @products = Product.all
+    @categories = Category.all
+  end
+
+  def show; end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit; end
+
+  def create
+    @product = Product.new product_params
+    if @product.save
+      flash[:success] = "Tao Product Thanh Cong"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      flash[:success] = "Xoa Product Thanh Cong"
+      redirect_to products_path
+    else
+      flash[:success] = "Xoa Product that bai"
+    end
+  end
+
+  def update
+    if @product.update_attributes product_params
+      flash[:success] = "Thay Doi Product Thanh Cong"
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def load_product
+    @product = Product.find_by params[:id]
+    return if @product
+    flash[:error] = "Khong tim thay product"
+    redirect_to products_path
+  end
+
+  def product_params
+    params.require(:product).permit :name, :price, :description, :status, :category_name
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,14 @@ class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :cartdetails, dependent: :destroy
   has_many :pictures, as: :imageable
+
+  enum status: {available: 0, not_exist: 1}
+
+  def category_name
+    category.try(:name)
+  end
+
+  def category_name=(name)
+    self.category = Category.find_or_create_by!(name: name) if name.present?
+  end
 end
